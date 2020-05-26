95033368d840751ea1d797ae9dc884515dba9f36
danieljuvito <danieljuvito@gmail.com>
2020-05-26 17:50:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-import IncludeLayout from './components/IncludeLayout.js'
+import Router from './Router.js';
+import './Counter.js';
 
-customElements.define('include-layout', IncludeLayout)
+const router = new Router({
+    mode: 'hash',
+    root: '/'
+});
+
+router
+    .add(/about/, () => {
+        document.title = 'About';
+        alert('welcome in about page');
+    })
+    .add(/products\/(.*)\/specification\/(.*)/, (id, specification) => {
+        document.title = 'Product';
+        alert(`products: ${id} specification: ${specification}`);
+    })
+    .add('', () => {
+        // general controller
+        document.title = 'Home';
+        console.log('welcome in catch all controller');
+    });
